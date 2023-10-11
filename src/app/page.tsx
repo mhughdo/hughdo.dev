cf90630bca8e652d354361174b7022f63b3cdc3d
Hugh Do <mhughdo@gmail.com>
2023-10-11 23:40:25 +0900

    feat: add fira code & roboto mono (#3)
    * feat: add fira code & roboto mono
    
    * style: remove space

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <main>Home</main>
+  return <main className='font-mono font-normal'>Home</main>
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,11 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['var(--font-fira-code)', 'var(--font-roboto-mono)'],
+      },
+    },
   },
   plugins: [],
 }
