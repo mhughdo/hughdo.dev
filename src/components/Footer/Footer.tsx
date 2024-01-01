6bf038fc9802f0527891a991c6e3671892b59549
Hugh Do <mhughdo@gmail.com>
2024-01-01 22:36:30 +0900

    feat: make footer responsive (#24)

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -22,13 +22,13 @@ const Footer = async () => {
 
   return (
     <footer className='mt-20 border-t dark:border-t-gray-600'>
-      <div className='wrapper mx-auto flex justify-between py-8'>
+      <div className='wrapper mx-auto flex flex-col items-center justify-between py-8 md:flex-row md:items-stretch'>
         <div className='flex flex-col justify-between'>
           <div>
             <Link href='/' className='text-primary-color text-2xl font-medium tracking-wider'>
               Hugh Do
             </Link>
-            <div className='mt-2 flex gap-4'>
+            <div className='mt-2 flex justify-center gap-4 md:justify-start'>
               {socialLinks.map((socialLink) => (
                 <Link
                   key={socialLink.name}
@@ -40,19 +40,22 @@ const Footer = async () => {
               ))}
             </div>
           </div>
-          <div>
+          <div className='hidden md:block'>
             <span className='text-secondary-color'>© 2024 Hugh Do —— Tokyo</span>
           </div>
         </div>
-        <div>
-          <p className='text-secondary-color'>Posts</p>
-          <div className='text-primary-color mt-2 grid grid-cols-2 gap-2'>
+        <div className='mt-4 md:mt-0'>
+          <p className='text-secondary-color text-center md:text-left'>Posts</p>
+          <div className='text-primary-color mt-2 grid grid-cols-2 justify-items-center gap-2 md:justify-items-start'>
             {categories.map((category) => (
               <Link key={category.slug} href={`/posts/${category.slug}`}>
                 {category.name}
               </Link>
             ))}
           </div>
+        </div>
+        <div className='mt-10 block md:hidden'>
+          <span className='text-secondary-color'>© 2024 Hugh Do —— Tokyo</span>
         </div>
       </div>
     </footer>
