b5ef4ba8a3b0a0c1cc99c9debc38c7d4607313ae
Hugh Do <mhughdo@gmail.com>
2024-01-01 23:22:38 +0900

    feat: render topics (#25)

---
+++ src/components/CategoryPill/CategoryPill.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import Link from 'next/link'
+
+import { Category } from '@/types'
+
+type CategoryPillProps = {
+  category: Category
+}
+
+const CategoryPill: FC<CategoryPillProps> = ({ category }) => {
+  return (
+    <Link
+      href={`/posts/${category.slug}`}
+      className='text-primary-color inline-block scale-[95%] rounded-lg bg-sky-200 px-2.5 py-1.5 text-sm font-medium opacity-80 transition duration-200 hover:scale-100 hover:opacity-100 md:px-2 md:py-1'>
+      {category.name}
+    </Link>
+  )
+}
+
+export default CategoryPill
--- src/components/Homepage/Topics.tsx
+++ src/components/Homepage/Topics.tsx
@@ -1,5 +1,25 @@
-const Topics = () => {
-  return <div className='text-center text-lg font-medium uppercase tracking-wider text-secondary'>Topics</div>
+import { FC } from 'react'
+
+import { Category } from '@/types'
+
+import CategoryPill from '../CategoryPill'
+
+type TopicsProps = {
+  className?: string
+  categories: Category[]
+}
+
+const Topics: FC<TopicsProps> = ({ categories }) => {
+  return (
+    <div>
+      <div className='text-center text-lg font-medium uppercase tracking-wider text-secondary'>Topics</div>
+      <div className='mt-6 space-x-1 space-y-1'>
+        {categories.map((category) => (
+          <CategoryPill key={category.slug} category={category} />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Topics
