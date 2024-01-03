41f5fa04952d4d785ff01260e1c36f7c2da6fc7b
Hugh Do <mhughdo@gmail.com>
2024-01-04 01:13:33 +0900

    feat: add ImageGrid & render some images (#30)
    * chore: Update framer-motion version to 10.17.3
    
    * feat: add ImageGrid & render some images

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,11 +1,17 @@
 import Banner from '@/components/Banner'
+import ImageGrid from '@/components/ImageGrid'
 import ProfileInfo from '@/components/ProfileInfo'
+import { getImages } from '@/helpers'
 
-const Photos = () => {
+export const revalidate = 3600
+
+const Photos = async () => {
+  const imageList = await getImages({})
   return (
     <div>
       <Banner />
       <ProfileInfo />
+      <ImageGrid imageList={imageList} />
     </div>
   )
 }
