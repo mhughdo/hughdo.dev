4988a1b9f1d51c84ceb00109aface840795d8a92
Hugh Do <mhughdo@gmail.com>
2024-01-15 19:01:35 +0900

    feat: change primary and secondary color in dark mode

--- src/components/Homepage/NewestPosts.tsx
+++ src/components/Homepage/NewestPosts.tsx
@@ -27,6 +27,3 @@ const NewestPosts: FC<NewestPostsProps> = ({ postsMetadata }) => {
 }
 
 export default NewestPosts
-
-// #1C3AFC, #4248ED, https://github.com/mhughdo/hughdo.dev/assets/15611134/7e6fbe90-ee18-41c3-9edc-b99f5c6d64b8
-// #ED1858, #F04230, #F03066
