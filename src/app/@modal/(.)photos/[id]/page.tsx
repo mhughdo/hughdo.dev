498204eb9fbbed68c9b9346b2e45ae762890d0dd
Hugh Do <mhughdo@gmail.com>
2024-01-15 12:32:06 +0900

    fix: fix build error

--- src/app/@modal/(.)photos/[id]/page.tsx
+++ src/app/@modal/(.)photos/[id]/page.tsx
@@ -7,7 +7,7 @@ import { findImageById, getImages } from '@/helpers'
 export const revalidate = 3600
 export async function generateStaticParams() {
   const images = await getImages()
-  return images.map((img) => ({ id: img.id }))
+  return images.map((img) => ({ id: img.id.toString() }))
 }
 
 const Page = async ({ params }: { params: { id: string } }) => {
